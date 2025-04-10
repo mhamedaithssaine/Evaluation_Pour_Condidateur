cd91a8dbc8902156b09faaf6cdf5992c9110c75e
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-10 21:48:12 +0100

    add CourseController with methode index,show

--- backend/app/Http/Controllers/CourseController.php
+++ backend/app/Http/Controllers/CourseController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\services\CourseServices;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    protected $courseService;
+
+    public function __construct(CourseServices $courseService){
+        $this->courseService=$courseService;
+    }
+
+    public function index() {
+        return response()->json($this->courseService->listCourses());
+    }
+
+    public function show($id) {
+        return response()->json($this->courseService->courseDetailes($id));
+    }
+
 }
