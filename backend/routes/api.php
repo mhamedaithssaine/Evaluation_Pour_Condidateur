55350022b238d4a2fa6ba0b2bca8f687c7bdf127
Mhamedaithssaine <mhamedaithssaine1@gmail.com>
2025-04-11 00:46:29 +0100

    add routes courses & subscrib & auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,12 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CourseController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/home', function () {
+    return response()->json([
+        'message' => 'Welcome to the API',
+        'laravel_version' => app()->version(),
+        'status' => 'success'
+    ]);
+});
 
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/register', [AuthController::class, 'registre']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
+    Route::post('/courses/{id}/subscribe', [CourseController::class, 'subscribe']);
+});
